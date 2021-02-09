6df0dd456e45a83a5feed975c859f25504f40663
Godcreatebugs <bhuvapiyush63@gmail.com>
2021-02-09 13:35:29 -0500

    strapi setup and gridsome setup

---
+++ e-vents-frontend/src/layouts/Default.vue
@@ -0,0 +1,17 @@
+<template>
+  <v-app>
+    <p>{{ $static.metadata.siteDescription }}</p>
+  <v-main>
+    <slot/>
+  </v-main>
+  </v-app>
+</template>
+
+<static-query>
+query {
+  metadata {
+    siteName,
+    siteDescription
+  }
+}
+</static-query>
