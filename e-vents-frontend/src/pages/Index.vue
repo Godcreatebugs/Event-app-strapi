569ca8944cb652c15622bc79d0cb19219b742224
Godcreatebugs <bhuvapiyush63@gmail.com>
2021-02-10 18:11:44 -0500

    finsihing strapi and gridsome

--- e-vents-frontend/src/pages/Index.vue
+++ e-vents-frontend/src/pages/Index.vue
@@ -1,64 +1,101 @@
 <template>
-  <Layout>
-    <v-container>
-     <v-row>
-       <v-col sm="6" offset-sm="4">
-        <v-tabs v-model="tab" grow>
-          <v-tab>Item One</v-tab>
-          <v-tab>Item two</v-tab>
-          <v-tab>Item Three</v-tab>
-      </v-tabs>
+  <Layout v-slot="{ searchText }">
+    <v-tabs v-model="tab" grow>
+      <v-tab>All Events</v-tab>
+      <v-tab>Live Music</v-tab>
+      <v-tab>Coding Events</v-tab>
+    </v-tabs>
+
+    <v-row class="justify-space-around">
       <v-card
-      class="mx-auto"
-      max-width="400"
-    >
-      <v-img
-        class="white--text align-end"
-        height="200px"
-        src="https://cdn.vuetifyjs.com/images/cards/docks.jpg"
+        v-for="edge in getEvents(searchText)" :key="edge.node.id"
+        width="280"
+        class="mt-5"
       >
-        <v-card-title>Top 10 Australian beaches</v-card-title>v-img>
+        <v-img
+          class="white--text align-end"
+          height="200px"
+          :src="`http://localhost:1337${edge.node.thumbnail}`"
+        />
 
-      <v-card-subtitle class="pb-0">
-        Number 10
-      </v-card-subtitle>
+        <v-card-title>{{ edge.node.title }}</v-card-title>
 
-      <v-card-text class="text--primary">
-        <div>Whitehaven Beach</div>
+        <v-card-subtitle class="pb-0">{{ formatDate(edge.node.date) }}</v-card-subtitle>
 
-        <div>Whitsunday Island, Whitsunday Islands</div>
-      </v-card-text>
-
-      <v-card-actions>
-        <v-btn
-          color="orange"
-          text
-        >
-          Share
-        </v-btn>
-
-        <v-btn
-          color="orange"
-          text
-        >
-          Explore
-        </v-btn>
-      </v-card-actions>
-    </v-card>
-    </v-col>
-  </v-row>
-   </v-container>
+        <v-card-actions>
+          <v-btn
+            @click="$router.push(`/events/${edge.node.id}`)"
+            color="orange"
+            text
+          >
+            More Info
+          </v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-row>
   </Layout>
 </template>
 
+<page-query>
+query {
+  events: allEvent {
+    edges {
+      node {
+				id
+        title
+        description
+        price
+        duration
+        date
+        thumbnail
+        image
+        category
+      }
+    }
+  }
+}
+</page-query>
+
 <script>
+import moment from 'moment'
 export default {
   metaInfo: {
     title: 'Hello, world!'
   },
-  data(){
+  data() {
     return {
-      tab: 0
+      tab: 0,
+      events: []
+    }
+  },
+  mounted() {
+    this.events = this.$page.events.edges
+  },
+  watch: {
+    tab(val) {
+      if (this.tab === 0) {
+        this.showAllEvents()
+      } else {
+        this.showEventsByType(val)
+      }
+    }
+  },
+  methods: {
+    showAllEvents() {
+      this.events = this.$page.events.edges
+    },
+    showEventsByType(val) {
+      this.events = this.$page.events.edges.filter((edge) => {
+        return edge.node.category === val
+      })
+    },
+    formatDate(date) {
+      return moment(date).format('MMMM Do YYYY, h:mm a')
+    },
+    getEvents(searchText) {
+      return this.events.filter((edge) => {
+        return edge.node.title.toLowerCase().includes(searchText.toLowerCase())
+      })
     }
   }
 }
